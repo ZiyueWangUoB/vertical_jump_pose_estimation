7b1423176646cb247c0dc1f5cf1a8e993596107a
Ziyue Wang <wangziyue@hotmail.co.uk>
2025-09-09 11:03:31 +0100

    adding vertical jump video demo with gitignore as well

--- vertical_jump_pose_estimation/examples/inference_image.py
+++ vertical_jump_pose_estimation/examples/inference_image.py
@@ -3,6 +3,7 @@ import imutils
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from vertical_jump_pose_estimation.examples.helpers import draw_points_on_img
 
 # Load model
 model = YOLO("yolo11s-pose.pt")
@@ -13,26 +14,7 @@ image = imutils.url_to_image(url)
 
 # Run inference
 results = model(image)
-
-# Define COCO keypoint skeleton (pairs of connected joints)
-skeleton = [
-    (0, 1), (0, 2), (1, 3), (2, 4),        # Head
-    (5, 6), (5, 7), (7, 9), (6, 8), (8, 10), # Arms
-    (5, 11), (6, 12), (11, 12),              # Torso
-    (11, 13), (13, 15), (12, 14), (14, 16)   # Legs
-]
-
-# Draw keypoints + skeleton
-for result in results:
-    if result.keypoints is not None:
-        keypoints = result.keypoints.xy.cpu().numpy()  # shape: (num_people, num_keypoints, 2)
-        for person in keypoints:
-            # Draw skeleton connections
-            for (i, j) in skeleton:
-                if i < len(person) and j < len(person):
-                    x1, y1 = person[i]
-                    x2, y2 = person[j]
-                    cv2.line(image, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 2)
+image = draw_points_on_img(results, image)
 
 # Show in matplotlib (so colors are correct)
 plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
